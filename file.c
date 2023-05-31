dc4fa53e17ddbfbd4e41c9fbe6edd38d1566e225
saibitinov <daniilsaibitinov@gmail.com>
2023-05-31 11:53:23 -0400

    Добавлена возможность умножения чисел

--- file.c
+++ file.c
@@ -4,7 +4,9 @@ int main(){
 	int b = 3;
 	int sum = a + b;
 	int difference = a - b;
+	int multiplier = a * b;
 	printf("Сумма: %d\n", sum); 
 	printf("Разность: %d\n", difference);
+	printf("Произведение: %d\n", multiplier)
 	return 0;
 }
